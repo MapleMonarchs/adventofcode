8c6aaef601bf1c720b5278dec26c3ec7d7598a74
= <=>
2022-12-08 09:33:15 +0100

    day 5 continued

--- 5/day5.c
+++ 5/day5.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include "stack.h"
 
 int main() {
-	printf("hallo");
-	printf("haaaa");
+
+	cstack* stack1 = &(cstack) {.pointer = -1};
+	cstack* stack2 = &(cstack) {.pointer = -1};
+	cstack* stack3 = &(cstack) {.pointer = -1};
+	cstack* stack4 = &(cstack) {.pointer = -1};
+	cstack* stack5 = &(cstack) {.pointer = -1};
+	cstack* stack6 = &(cstack) {.pointer = -1};
+	cstack* stack7 = &(cstack) {.pointer = -1};
+	cstack* stack8 = &(cstack) {.pointer = -1};
+	cstack* stack9 = &(cstack) {.pointer = -1};
+
+	cstckpsha(stack1, (char* []){ "H", "T", "Z", "D" }, 										4);
+	cstckpsha(stack2, (char* []){ "Q", "R", "W", "T", "G", "C", "S"}, 			7);
+	cstckpsha(stack3, (char* []){ "P", "B", "F", "Q", "N", "R", "C", "H" }, 8);
+	cstckpsha(stack4, (char* []){ "L", "C", "N", "F", "H", "Z" }, 					6);
+	cstckpsha(stack5, (char* []){ "G", "L", "F", "Q", "S" }, 								5);
+	cstckpsha(stack6, (char* []){ "V", "P", "W", "Z", "B", "R", "C", "S" }, 8);
+	cstckpsha(stack7, (char* []){ "Z", "F", "J" }, 													3);
+	cstckpsha(stack8, (char* []){ "D", "L", "V", "Z", "R", "H", "Q" }, 			7);
+	cstckpsha(stack9, (char* []){ "B", "H", "G", "N", "F", "Z", "L", "D" }, 8);
+
+	printf("hallo\n");
+	printf("haaaa\n");
 }
--- 5/stack.h
+++ 5/stack.h
@@ -1,5 +1,25 @@
+#include <err.h>
+
 typedef struct cstack {
-	char content;
-	cstack* next;
+	char* contents[100];
+	int pointer;
 } cstack;
 
+void cstckpsh(cstack* stack, char* element) {
+
+	stack->pointer += 1;
+	stack->contents[stack->pointer] = element;
+}
+
+void cstckpsha(cstack* stack, char* elements[], int length){
+	for(int i = 0; i < length-1; i++) {
+		cstckpsh(stack, elements[i]);
+	}
+}
+
+char* cstckpp(cstack* stack) {
+	(stack->pointer < 0) ? errx(69, "Stack underflow!") : 0;
+	
+	stack->pointer -= 1;
+	return stack->contents[stack->pointer + 1]; 
+}
